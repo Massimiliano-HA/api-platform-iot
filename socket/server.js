f1107d7838b0c111f46b5bde8c82294c1a257b1c
Elim Florvil <elim.florvil@icloud.com>
2024-01-25 09:32:14 +0100

    refactor

--- socket/server.js
+++ socket/server.js
@@ -1,188 +1,195 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const { getUser } = require("./storage");
+const SerialPort = require("serialport");
+const xbee_api = require("xbee-api");
+const mqtt = require("mqtt");
+const dotenv = require("dotenv");
 
-var SerialPort = require("serialport");
-var xbee_api = require("xbee-api");
-var C = xbee_api.constants;
-require("dotenv").config();
+dotenv.config();
 
+// Configuration MQTT
+const brokerUrl = "mqtt://mqtt-dashboard.com";
+const clientName = "mqtt-node-client";
+const options = {
+  clientId: clientName,
+};
+const client = mqtt.connect(brokerUrl, options);
+
+// Configuration XBee
 const SERIAL_PORT = process.env.SERIAL_PORT;
-
-var xbeeAPI = new xbee_api.XBeeAPI({
-  api_mode: 2,
-});
-
-let serialport = new SerialPort(
+const xbeeAPI = new xbee_api.XBeeAPI({ api_mode: 2 });
+const serialport = new SerialPort(
   SERIAL_PORT,
   {
     baudRate: parseInt(process.env.SERIAL_BAUDRATE) || 9600,
   },
-  function (err) {
-    if (err) {
-      return console.log("Error: ", err.message);
-    }
-  }
+  handleSerialPortError
 );
 
-// Const pour le mqtt ----------------------------------
-const mqtt = require("mqtt");
-
-const brokerUrl = "mqtt://mqtt-dashboard.com";
-const tcpPort = 1883;
-const wsPort = 8083;
-const securePort = 8883;
-const secureWsPort = 8084;
-
-const clientName = "mqtt-node-client";
-
-const options = {
-  clientId: clientName,
-};
-
-const client = mqtt.connect(brokerUrl, options);
-
+// Variables globales
 let valeurAD1;
 let dataReceived;
+let user;
+const uid = "dru7DyoWEkTX17twZP9f49O18ED3";
+let movedDetected = false;
 
+// Fonction pour gérer les erreurs du port série
+function handleSerialPortError(err) {
+  if (err) {
+    console.log("Error: ", err.message);
+  }
+}
+
+// Fonction pour envoyer une commande à distance
 function sendRemoteCommand(command) {
   try {
     const frame = xbeeAPI.buildFrame(command);
     serialport.write(frame);
   } catch (error) {
-    console.error('Erreur lors de la construction de la trame XBee :', error);
+    console.error("Erreur lors de la construction de la trame XBee :", error);
   }
 }
 
-
-
+// Gestion des événements sur le port série
 serialport.pipe(xbeeAPI.parser);
 xbeeAPI.builder.pipe(serialport);
 
-
-
 serialport.on("open", function () {
-  var frame_obj = {
+  // Initialisation des trames XBee
+  const frameObj1 = {
     type: C.FRAME_TYPE.AT_COMMAND,
     command: "NI",
     commandParameter: [],
   };
+  xbeeAPI.builder.write(frameObj1);
 
-  xbeeAPI.builder.write(frame_obj);
-
-  frame_obj = {
+  const frameObj2 = {
     type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
     destination64: "FFFFFFFFFFFFFFFF",
     command: "NI",
     commandParameter: [],
   };
-  xbeeAPI.builder.write(frame_obj);
+  xbeeAPI.builder.write(frameObj2);
 
-  // Récuperer les infos du capteur photon (mettre adc) ----------------------------------
-  xbeeAPI.parser.on("data", function (frame) {
-
-    if (C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET === frame.type) {
-      // Récupère les données RF
-      let dataReceived = "";
-      for (let i = 0; i < frame.data.length; i++) {
-        dataReceived += String.fromCharCode(frame.data[i]);
-      }
-      dataReceived = dataReceived.trim();
-      console.log(">> ZIGBEE_RECEIVE_PACKET >", dataReceived);
-
-      // Utilise un switch pour gérer différents types de paquets en fonction des données RF
-      switch (dataReceived) {
-        case "MOTION_DETECTED":
-          console.log("Paquet de détection de mouvement");
-          // TODO : Envoyer un message MQTT IF motion detected AND STATE  = 0
-          break;
-        default:
-          console.log("Code ");
-          console.log(dataReceived);
-      }
-    }
-
-
-    if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
-      // let dataReceived = String.fromCharCode.apply(null, frame.nodeIdentifier);
-      console.log("NODE_IDENTIFICATION");
-      //storage.registerSensor(frame.remote64)
-
-    } else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
-
-      console.log("ZIGBEE_IO_DATA_SAMPLE_RX")
-      console.log(frame.analogSamples.AD0)
-      //storage.registerSample(frame.remote64,frame.analogSamples.AD0 )
-
-    } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
-      // console.log("REMOTE_COMMAND_RESPONSE")
-    } else {
-      console.debug(frame);
-      let dataReceived = String.fromCharCode.apply(null, frame.commandData)
-      // console.log(dataReceived);
-    }
-
-
-    if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
-      if (frame.analogSamples && frame.analogSamples.AD1 !== undefined) {
-        valeurAD1 = frame.analogSamples.AD1;
-        console.log("Valeur du capteur AD1 (D1) :", valeurAD1);
-
-        //Envoyer une commande pour allumer la LED si valeurAD1 est entre 40 et 50
-        if (valeurAD1 >= 40 && valeurAD1 <= 50) {
-          const remoteCommand = {
-            type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
-            destination64: "FFFFFFFFFFFFFFFF",
-            command: "D0",
-            commandParameter: [0x05],
-          };
-          console.log('Rien à signaler !');
-          sendRemoteCommand(remoteCommand);
-        } else {
-          const remoteCommand = {
-            type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
-            destination64: "FFFFFFFFFFFFFFFF",
-            command: "D0",
-            commandParameter: [0x04],
-          };
-          console.log('Intrusion ! Lumiere allumée !');
-          sendRemoteCommand(remoteCommand);
-        }
-
-      }
-    }
-  });
+  // Récupération des infos du capteur photon (mettre adc)
+  xbeeAPI.parser.on("data", handleXBeeData);
 });
 
-client.on('connect', () => {
-  console.log('Connecté au serveur MQTT');
+// Gestion des données XBee
+function handleXBeeData(frame) {
+  if (C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET === frame.type) {
+    handleZigbeeReceivePacket(frame);
+  }
 
-  client.subscribe('object ', (err) => {
+  if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
+    console.log("NODE_IDENTIFICATION");
+  } else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
+    console.log("ZIGBEE_IO_DATA_SAMPLE_RX");
+    console.log(frame.analogSamples.AD0);
+  } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
+    // console.log("REMOTE_COMMAND_RESPONSE")
+  } else {
+    console.debug(frame);
+    let dataReceived = String.fromCharCode.apply(null, frame.commandData);
+    // console.log(dataReceived);
+  }
+
+  if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
+    handleAD1Value(frame);
+  }
+}
+
+// Gestion des paquets Zigbee Receive
+function handleZigbeeReceivePacket(frame) {
+  let dataReceived = "";
+  for (let i = 0; i < frame.data.length; i++) {
+    dataReceived += String.fromCharCode(frame.data[i]);
+  }
+  dataReceived = dataReceived.trim();
+  console.log(">> ZIGBEE_RECEIVE_PACKET >", dataReceived);
+
+  switch (dataReceived) {
+    case "MOTION_DETECTED":
+      if (user.alarmState) {
+        console.log("Alarme activée");
+        movedDetected = true;
+        publishDataToTopic(client, "object", "Mouvement détecté !");
+      }
+      break;
+    default:
+      if (user.alarmState && movedDetected) {
+        console.log("Code reçu :", dataReceived);
+        movedDetected = true;
+        publishDataToTopic(client, "object", "Mouvement détecté !");
+      }
+      console.log("Code ");
+      console.log(dataReceived);
+  }
+}
+
+// Gestion de la valeur AD1
+function handleAD1Value(frame) {
+  if (frame.analogSamples && frame.analogSamples.AD1 !== undefined) {
+    valeurAD1 = frame.analogSamples.AD1;
+    console.log("Valeur du capteur AD1 (D1) :", valeurAD1);
+
+    const remoteCommand = {
+      type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+      destination64: "FFFFFFFFFFFFFFFF",
+      command: "D0",
+      commandParameter: valeurAD1 >= 40 && valeurAD1 <= 50 ? [0x05] : [0x04],
+    };
+
+    console.log(
+      valeurAD1 >= 40 && valeurAD1 <= 50
+        ? "Rien à signaler !"
+        : "Intrusion ! Lumière allumée !"
+    );
+    sendRemoteCommand(remoteCommand);
+  }
+}
+
+// Gestion des événements MQTT
+client.on("connect", handleMQTTConnect);
+client.on("message", handleMQTTMessage);
+client.on("close", () => console.log("Déconnecté du serveur MQTT"));
+client.on("error", (err) => console.error("Erreur MQTT:", err));
+
+// Fonction pour gérer la connexion MQTT
+function handleMQTTConnect() {
+  console.log("Connecté au serveur MQTT");
+  client.subscribe("object", (err) => {
     if (!err) {
       console.log('Abonné au sujet (topic) "object"');
     }
-
     setInterval(() => {
-      // Valeur du digicode
+      console.log(user);
       if (dataReceived !== undefined) {
-        publishDataToTopic('object', dataReceived);
+        // publishDataToTopic(client, 'object', dataReceived);
       } else {
-        console.log('Pas de valeur du digicode');
+        console.log("Pas de valeur du digicode");
+        // publishDataToTopic(client, "object", "Pas de valeur du digicode");
       }
-
-      //console.log(valeurAD1 + ' ' + dataReceived);
-
     }, 7000);
   });
-});
+}
 
-client.on('message', (topic, message) => {
-  console.log(`Message reçu du sujet (topic) "${topic}": ${message.toString()}`);
-});
+// Fonction pour gérer les messages MQTT
+function handleMQTTMessage(topic, message) {
+  console.log(
+    `Message reçu du sujet (topic) "${topic}": ${message.toString()}`
+  );
+}
 
-client.on('close', () => {
-  console.log('Déconnecté du serveur MQTT');
-});
+// Fonction pour publier des données sur un sujet MQTT
+function publishDataToTopic(client, topic, message) {
+  client.publish(topic, message, (err) => {
+    if (err) {
+      console.error("Erreur lors de la publication du message:", err);
+    } else {
+      console.log(`Message publié sur le sujet "${topic}": ${message}`);
+    }
+  });
+}
 
-client.on('error', (err) => {
-  console.error('Erreur MQTT:', err);
-});
+// Exemple d'utilisation de la fonction getUser
+user = getUser(uid);
--- socket/storage.js
+++ socket/storage.js
@@ -1,49 +1,65 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
 
+// Initialiser l'application Firebase
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://homewave-45d37.firebaseio.com",
 });
 
-const db = admin.firestore();
+async function getUser(uid) {
+  try {
+    const firestore = admin.firestore();
+    const userRef = firestore.collection("users").doc(uid);
 
+    const userSnapshot = await userRef.get();
 
-module.exports.registerSensor = async function (address) {
-
-  const docRef = db.collection('sensors').doc(address);
-
-  const sensor = {
-    address: address,
-    date: Date.now(),
+    // Vérifier si le document existe dans Firestore
+    if (userSnapshot.exists) {
+      console.log("Utilisateur existant dans Firestore");
+      console.log(userSnapshot.data());
+      return userSnapshot.data();
+    } else {
+      console.log("Création en cours...");
+      return await createUser(uid);
+    }
+  } catch (error) {
+    console.error("Erreur lors de la récupération de l'utilisateur:", error);
+    throw error;
   }
-
-  await docRef.get().then((snapshotDoc)=> {
-    if (!snapshotDoc.exists)
-      docRef.set(sensor);
-    else
-      docRef.update(sensor);
-  })
 }
 
-module.exports.registerSample = async function (address, sample) {
+// Créer un utilisateur dans Firestore
+async function createUser(uid) {
+  try {
+    const firestore = admin.firestore();
+    const userRef = firestore.collection("users").doc(uid);
 
-  const docRef = db.collection('sensors').doc(address)
-    .collection('samples').doc(Date.now().toString());
+    const userData = {
+      alarmState: true,
+      system: [
+        {
+          type: "digicode",
+          name: "codePin",
+          value: "0000",
+        },
+      ],
+      events: [
+        {
+          date: Date.now(),
+          type: "event",
+        },
+      ],
+    };
 
-  const data = {
-    value: sample,
-    date: Date.now(),
+    await userRef.set(userData);
+    return userData;
+  } catch (error) {
+    console.error("Erreur lors de la création de l'utilisateur:", error);
+    throw error;
   }
-  await docRef.set(data);
-
-
 }
 
-module.exports.listSensors = function () {
-
-  const docRef = db.collection('sensors');
-
-  return docRef.get()
-
-}
-
+module.exports = {
+  getUser,
+};
